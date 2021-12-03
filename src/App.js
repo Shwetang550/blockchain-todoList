35dc3a14cc0405c5429b855f256e7e147397164f
Shwetang550 <shwetangsingh5@gmail.com>
2021-12-03 13:34:29 +0530

    react-app boiler modif + todo input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [todo, setTodo] = useState('');
+
+  const handleInputTodo = (event) => setTodo(event.target.value)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <input
+        placeholder='type your todos here...'
+        onChange={handleInputTodo}
+        value={todo}
+      />
+    </>
+  )
 }
 
-export default App;
+export default App
